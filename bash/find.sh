b23a0108321691d51d02c44fc54b842749b14821
koban4eg <knyaznikolai@gmail.com>
2012-10-01 15:26:47 +0400

    -

--- bash/find.sh
+++ bash/find.sh
@@ -3,15 +3,8 @@
 myfind(){
 for i in $(ls $1)
 do
-    target=`echo "$1/$i" | tr [:lower:] [:upper:]`
-    sample=`echo "$1/$2" | tr [:lower:] [:upper:]`
-    #echo TARGET $1/$i SAMPLE $1/$2
     check=`echo "$1/$2" | grep -io "$1/$i"`  
-
-    if [ $target = $sample ] && [ -$3 $1/$i ]
-    then	
-        echo $1/$i
-    fi
+    [ $check ] && [ -$3 $1/$i ] && echo $1/$i
     [ -d $1/$i ] && myfind $1/$i $2 $3
 done
 }
